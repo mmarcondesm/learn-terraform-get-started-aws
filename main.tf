de3f32167c853289ab74a2638ad9f7a5b3910c09
mmarcondesm <matheus.marcondes@outlook.com>
2025-11-17 13:00:15 -0300

    Add files via upload

--- main.tf
+++ main.tf
@@ -4,44 +4,51 @@ provider "aws" {
 }
 
 data "aws_ami" "ubuntu" {
-  most_recent = true
+  most_recent = true  
 
   filter {
-    name   = "name"
+    name = "name"
     values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
   }
 
-  owners = ["099720109477"] # Canonical
+  owners = ["099720109477"] 
 }
 
 resource "aws_instance" "app_server" {
-  ami           = data.aws_ami.ubuntu.id
+   ami           = data.aws_ami.ubuntu.id
   instance_type = var.instance_type
 
   vpc_security_group_ids = [module.vpc.default_security_group_id]
   subnet_id              = module.vpc.private_subnets[0]
 
   tags = {
-    Name = var.instance_name
+   Name = var.instance_name
   }
-
-
-
-  module "vpc" {
-    source  = "terraform-aws-modules/vpc/aws"
-    version = "5.19.0"
-
-
-    name = "example-vpc"
-    cidr = "10.0.0.0/16"
-
-
-    azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
-    private_subnets = ["10.0.0.0/24", "10.0.2.0/24"]
-    public_subnet   = ["10.0.101.0/24"]
-
-    enable_dns_hostnames = true
-  }
-
 
 }
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.19.0"
+
+  name = "example-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets  = ["10.0.101.0/24"]
+
+  enable_dns_hostnames    = true
+}
+
+
+resource "aws_s3_account_public_access_block" "this" {
+  count = var.create ? 1 : 0
+  account_id = var.account_id
+
+  block_public_acls       = var.block_public_acls
+  block_public_policy     = var.block_public_policy
+  ignore_public_acls      = var.ignore_public_acls
+  restrict_public_buckets = var.restrict_public_buckets
+}
+
--- output.tf
+++ output.tf
@@ -2,3 +2,7 @@ output "instance_hostname" {
   description = "Private DNS name of the EC2 instance."
   value       = aws_instance.app_server.private_dns
 }
+output "s3_account_public_access_block_id" {
+  description = "AWS account ID"
+  value = try(aws_s3_account_public_access_block.this[0].id, "")
+}
